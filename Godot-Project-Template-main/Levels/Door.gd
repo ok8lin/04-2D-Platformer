09fbd780f27f65316ec24a0875ed6a9f5610ab76
ok8lin <harriskn@iu.edu>
2021-10-23 15:04:01 -0400

    Completed Project
    2D Platformer

---
+++ Godot-Project-Template-main/Levels/Door.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready():
+	pass 
+
+
+
+func _on_Door_body_entered(body):
+	if Global.save_data["level"] == 1:
+		Global.save_data["level"] = 2
+		var _s = get_tree().change_scene("res://Levels/Level2.tscn")
+
